86b2c67ec600d2e0642c58d1840188e1a46e9b00
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-10-17 16:40:15 -0500

    .

--- lexyc/constants.go
+++ lexyc/constants.go
@@ -3,6 +3,7 @@ package lexyc
 import (
 	"go-custom-compiler/models"
 	"log"
+	"regexp"
 	"strings"
 )
 
@@ -23,8 +24,8 @@ func (l *LexicalAnalyzer) NextConstant(currentLine string, lineIndex int64, debu
 
 		if l.R.RegexConstanteEntera.MatchEnteraConstantDeclaration(currentLine) {
 			currentLine = strings.TrimSuffix(currentLine, ";")
-			floatData := strings.Split(currentLine, ":=")
-			l.ConstantStorage = append(l.ConstantStorage, models.Token{Type: models.ENTERO, Key: floatData[0], Value: floatData[1]})
+			constantData := strings.Split(currentLine, ":=")
+			l.ConstantStorage = append(l.ConstantStorage, models.Token{Type: models.ENTERO, Key: constantData[0], Value: constantData[1]})
 			l.GL.Printf("%+v[CONSTANT] Entero Found > %+v", funcName, currentLine)
 
 			if debug {
@@ -32,10 +33,11 @@ func (l *LexicalAnalyzer) NextConstant(currentLine string, lineIndex int64, debu
 			}
 			return
 		}
+
 		if l.R.RegexConstanteReal.MatchRealConstantDeclaration(currentLine) {
 			currentLine = strings.TrimSuffix(currentLine, ";")
-			floatData := strings.Split(currentLine, ":=")
-			l.ConstantStorage = append(l.ConstantStorage, models.Token{Type: models.REAL, Key: floatData[0], Value: floatData[1]})
+			constantData := strings.Split(currentLine, ":=")
+			l.ConstantStorage = append(l.ConstantStorage, models.Token{Type: models.REAL, Key: constantData[0], Value: constantData[1]})
 			l.GL.Printf("%+v[CONSTANT] Real Found > %+v", funcName, currentLine)
 
 			if debug {
@@ -43,10 +45,11 @@ func (l *LexicalAnalyzer) NextConstant(currentLine string, lineIndex int64, debu
 			}
 			return
 		}
+
 		if l.R.RegexConstanteAlfabetica.MatchAlfabeticaConstantDeclaration(currentLine) {
 			currentLine = strings.TrimSuffix(currentLine, ";")
-			floatData := strings.Split(currentLine, ":=")
-			l.ConstantStorage = append(l.ConstantStorage, models.Token{Type: models.ALFABETICO, Key: floatData[0], Value: floatData[1]})
+			constantData := strings.Split(currentLine, ":=")
+			l.ConstantStorage = append(l.ConstantStorage, models.Token{Type: models.ALFABETICO, Key: constantData[0], Value: constantData[1]})
 			l.GL.Printf("%+v[CONSTANT] Alfabetico Found > %+v", funcName, currentLine)
 
 			if debug {
@@ -54,16 +57,69 @@ func (l *LexicalAnalyzer) NextConstant(currentLine string, lineIndex int64, debu
 			}
 			return
 		}
+
 		if l.R.RegexConstanteLogica.MatchLogicaConstantDeclaration(currentLine) {
 			currentLine = strings.TrimSuffix(currentLine, ";")
-			floatData := strings.Split(currentLine, ":=")
-			l.ConstantStorage = append(l.ConstantStorage, models.Token{Type: models.LOGICO, Key: floatData[0], Value: floatData[1]})
+			constantData := strings.Split(currentLine, ":=")
+			l.ConstantStorage = append(l.ConstantStorage, models.Token{Type: models.LOGICO, Key: constantData[0], Value: constantData[1]})
 			l.GL.Printf("%+v[CONSTANT] Logico Found > %+v", funcName, currentLine)
 
 			if debug {
 				log.Printf("[CONSTANT] Logico Found > %+v", currentLine)
 			}
 			return
+		}
+
+		if l.R.RegexConstanteDefault.MatchVariableDefault(currentLine) {
+			currentLine = strings.TrimSuffix(currentLine, ";")
+			constantData := strings.Split(currentLine, ":=")
+			value := constantData[1]
+			l.ConstantStorage = append(l.ConstantStorage, models.Token{Type: models.LOGICO, Key: constantData[0], Value: value})
+
+			// regexEntero := regexp.MustCompile(`([0-9]+|\-[0-9]+)`)
+			// regexReal := regexp.MustCompile(`(([0-9]+|\-[0-9]+)\.([0-9]+)|([0-9]+|\-[0-9]+)e[0-9]+)`)
+			regexLogico := regexp.MustCompile(`((?i)verdadero|(?i)falso))`)
+			// regexAlfabetico := regexp.MustCompile(`(\s*)((\"(\w)*\")`)
+
+			if regexLogico.MatchString(value) {
+				foundTypo := false
+				keyword := "verdadero"
+				keyData := strings.Split(keyword, "")
+				for i, char := range value {
+					if i < len(keyData)-1 {
+						if !foundTypo {
+							if string(char) != keyData[i] {
+								foundTypo = true
+								if debug {
+									log.Printf("[ERR] Found typo in '%+v' declaration at [%+v] on line [%+v]. Correct syntax should be '%+v'", value, i, lineIndex, keyword)
+								}
+								l.GL.Printf("[ERR] Found typo in '%+v' declaration at [%+v] on line [%+v]. Correct syntax should be '%+v'", value, i, lineIndex, keyword)
+							}
+						}
+					}
+				}
+				if foundTypo {
+					return
+				}
+				keyword = "falso"
+				for i, char := range value {
+					if i < len(keyData)-1 {
+						if !foundTypo {
+							if string(char) != keyData[i] {
+								foundTypo = true
+								if debug {
+									log.Printf("[ERR] Found typo in '%+v' declaration at [%+v] on line [%+v]. Correct syntax should be '%+v'", value, i, lineIndex, keyword)
+								}
+								l.GL.Printf("[ERR] Found typo in '%+v' declaration at [%+v] on line [%+v]. Correct syntax should be '%+v'", value, i, lineIndex, keyword)
+							}
+						}
+					}
+				}
+				if foundTypo {
+					return
+				}
+			}
+
 		}
 
 		l.GL.Printf("%+v Did not found any type of match on Line[%+v]! ", funcName, lineIndex)
