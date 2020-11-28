46cf5d9aacb4f3fcd725a5e1303ca25082bf6cfb
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-11-27 20:01:25 -0600

    UPDATE: Check redeclarations entero

---
+++ lexyc/local_helper.go
@@ -0,0 +1,33 @@
+package lexyc
+
+import "go-custom-compiler/models"
+
+//DoesTheTokenExistsInGlobalVariables ...
+func (l *LexicalAnalyzer) DoesTheTokenExistsInGlobalVariables(token *models.Token) bool {
+	for _, each := range l.VariableStorage {
+		if each.Key == token.Key {
+			return true
+		}
+	}
+	return false
+}
+
+//DoesTheTokenExistsInGlobalConstants ...
+func (l *LexicalAnalyzer) DoesTheTokenExistsInGlobalConstants(token *models.Token) bool {
+	for _, each := range l.ConstantStorage {
+		if each.Key == token.Key {
+			return true
+		}
+	}
+	return false
+}
+
+//DoesTheTokenExistsInLocalVariables ...
+func (l *LexicalAnalyzer) DoesTheTokenExistsInLocalVariables(token *models.Token, function *models.TokenFunc) bool {
+	for _, each := range function.Vars {
+		if each.Key == token.Key {
+			return true
+		}
+	}
+	return false
+}
